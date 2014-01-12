d526734565606ddba50cd0c51783a719d9bd5b91
Alexander Nicolaysen Sørnes <alexsornes@gmail.com>
2014-01-12 15:44:36 +0100

    testData: Avoid deprecated mktime()

--- include/testData.php
+++ include/testData.php
@@ -1484,7 +1484,7 @@ class testData{
         if(!$this->iTestingId)
             return false;
 
-        return ((mktime() - mysqltimestamp_to_unixtimestamp($this->sSubmitTime)) > (60 * 60 * 24  * TESTDATA_AGED_THRESHOLD));
+        return ((time() - mysqltimestamp_to_unixtimestamp($this->sSubmitTime)) > (60 * 60 * 24  * TESTDATA_AGED_THRESHOLD));
     }
 
     public function objectSetParent($iNewId, $sClass = 'version')
