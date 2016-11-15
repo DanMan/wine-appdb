aaca627ffd5ff8aff345d5e6d11bbf4f37a01177
Jeremy Newman <jnewman@codeweavers.com>
2016-11-15 12:13:17 -0600

    add some comments to explain what this file is

--- comment_body.php
+++ comment_body.php
@@ -1,8 +1,12 @@
 <?php
+/**
+ * Ajax Loader for Loading an Inline Comment
+ * ties to handler in utils.js
+ */
+
 require("path.php");
 require(BASE."include/incl.php");
 require_once(BASE."include/comment.php");
-
 
 Comment::view_comment_body($aClean['iCommentId']);
 
