52340938a0d42d199943af22ecdc4b43d7de14e3
Jeremy Newman <jnewman@codeweavers.com>
2016-11-15 12:15:59 -0600

    move appdb specific functions to appdb.php, cleanup
     * add error handing routines
     * add debug log
     * merge some one time used functions

--- include/appdb.php
+++ include/appdb.php
@@ -1,41 +1,337 @@
 <?php
+/*************************************************/
+/* Application Database Global Functions         */
+/*************************************************/
 
-function log_category_visit($catId)
+/*
+ * TODO:
+ *   + replace all apidb_ calls with appdb_
+ *   + apidb_url and apidb_fullurl should be merged
+ *   + addmsg should be removed, and calls replaced with session->addmsg
+ */
+
+/**
+ * log debug message to debug console
+ */
+function appdb_debug($msg = "")
 {
-    global $REMOTE_ADDR;
+    if (APPDB_DEBUG == 1 or $GLOBALS['_APPDB_debug']) {
+        $GLOBALS['_APPDB_debugLog'] .= "[".date("D M j G:i:s Y",time())."] ".$msg."\n";
+    }
+    return;
+}
 
-    $result = query_parameters("SELECT * FROM catHitStats WHERE ip = '?' AND catId = '?'",
-                               $REMOTE_ADDR, $catId);
-    if($result && query_num_rows($result) == 1)
+/**
+ * output a debug string so it appears above all the webpage content
+ * useful for seeing vars and objects in real time
+ */
+function appdb_dump($var = "")
+{
+    echo '<div class="appdb_dump"><xmp>';
+    var_dump($var);
+    echo "</xmp></div>\n";
+}
+
+/**
+ * shutdown handler (FATAL errors)
+ */
+function appdb_error_shutdown()
+{
+    // get information about last error
+    $error = error_get_last();
+    // handle error if defined
+    if (!empty($error))
     {
-        $oStatsRow = query_fetch_object($result);
-        query_parameters("UPDATE catHitStats SET count = count + 1 WHERE catHitId = '?'",
-                         $oStatsRow->catHitId);
-    } else
-    {
-        query_parameters("INSERT INTO catHitStats (appHitId, time, ip, catId, count) ".
-                         "VALUES (?, ?, '?', '?', '?')",
-                         "null", "null", $REMOTE_ADDR, $catId, "1");
+        // output error
+        appdb_error_handler($error['type'], $error['message'], $error['file'], $error['line']);
     }
 }
 
-function log_application_visit($appId)
+/**
+ * error handler
+ */
+function appdb_error_handler($errno, $errstr, $errfile, $errline, $errcontext = null)
 {
-    global $REMOTE_ADDR;
+    global $config;
 
-    $result = query_parameters("SELECT * FROM appHitStats WHERE ip = '?' AND appId = '?'",
-                               $REMOTE_ADDR, $appId);
-    if($result && query_num_rows($result) == 1)
+    // debug text
+    $debug = "";
+
+    // PHP error codes
+    $err = array(
+                 1    => 'E_ERROR',
+                 2    => 'E_WARNING',
+                 4    => 'E_PARSE',
+                 8    => 'E_NOTICE',
+                 16   => 'E_CORE_ERROR',
+                 32   => 'E_CORE_WARNING',
+                 64   => 'E_COMPILE_ERROR',
+                 128  => 'E_COMPILE_WARNING',
+                 256  => 'E_USER_ERROR',
+                 512  => 'E_USER_WARNING',
+                 1024 => 'E_USER_NOTICE',
+                 2047 => 'E_ALL',
+                 2048 => 'E_STRICT',
+                 4096 => 'E_RECOVERABLE_ERROR',
+                 8192 => 'E_DEPRECATED'
+                );
+
+    // clean up errfile
+    $errfile = basename($errfile);
+
+    // log PHP non-fatal code errors
+    if ($errno == E_NOTICE or $errno == E_USER_NOTICE or $errno == E_USER_WARNING or $errno == E_STRICT or $errno == E_DEPRECATED)
+        appdb_debug("error", "ERROR:[{$err[$errno]}] {$errfile}:{$errline} - {$errstr}");
+
+    // do not fail on PHP STRICT or DEPRECIATED code on production site
+    if ($errno and ($errno == E_STRICT or $errno == E_DEPRECATED) and !$config->test_mode)
+        return;
+
+    // don't exit on a notice or warning
+    if ($errno == E_NOTICE or $errno == E_WARNING)
+        return;
+
+    // write to the error log
+    error_log("[".date("D M j G:i:s Y",time())."] [".$err[$errno]."] ".$errfile.":".$errline." - ".$errstr."\n");
+
+    // don't exit on a USER notice or warning, but log above
+    if ($errno == E_USER_NOTICE or $errno == E_USER_WARNING)
+        return;
+
+    // show additional debug output
+    $debug = "";
+    if (APPDB_DEBUG === "1" and function_exists('debug_backtrace'))
     {
-        $stats = query_fetch_object($result);
-        query_parameters("UPDATE appHitStats SET count = count + 1 WHERE appHitId = '?'",
-                         $stats->appHitId);
-    } else
-    {
-        query_parameters("INSERT INTO appHitStats (appHitId, time, ip, appId, count) ".
-                         "VALUES (?, ?, '?', '?', '?')",
-                         "null", "null", $REMOTE_ADDR, $appId, "1");
+        // build context
+        $ctx = '';
+        if (!empty($errcontext))
+        {
+            foreach ($errcontext as $key => $value)
+            {
+                switch (gettype($value))
+                {
+                    case "string":
+                        $ctx .= "[$key] => $value\n";
+                        break;
+                    default:
+                        $ctx .= "[$key] => ".gettype($value)."\n";
+                }
+            }
+            if (!empty($ctx))
+                $debug = "<b>Context</b>: \n".$ctx."\n\n";
+        }
+
+        // build backtrace
+        $backtrace = debug_backtrace();
+        $output = "";
+        foreach ($backtrace as $bt)
+        {
+           $args = '';
+           if (isset($bt['args']))
+           {
+               foreach ($bt['args'] as $a)
+               {
+                   if (!empty($args))
+                       $args .= ', ';
+                   switch (gettype($a))
+                   {
+                       case 'integer':
+                       case 'double':
+                           $args .= $a;
+                           break;
+                       case 'string':
+                           $a = $this->encode(substr($a, 0, 64)).((strlen($a) > 64) ? '...' : '');
+                           $args .= "\"$a\"";
+                           break;
+                       case 'array':
+                           $args .= 'Array('.count($a).')';
+                           break;
+                       case 'object':
+                           $args .= 'Object('.get_class($a).')';
+                           break;
+                       case 'resource':
+                           $args .= 'Resource('.strstr($a, '#').')';
+                           break;
+                       case 'boolean':
+                           $args .= $a ? 'True' : 'False';
+                           break;
+                       case 'NULL':
+                           $args .= 'Null';
+                           break;
+                       default:
+                           $args .= 'Unknown';
+                   }
+               }
+           }
+           if (isset($bt['file']))
+               $output .= "<b>file</b>: {$bt['file']}\n";
+           if (isset($bt['line']))
+               $output .= "<b>line</b>: {$bt['line']}\n";
+           if (isset($bt['class']))
+               $output .= "<b>call</b>: {$bt['class']}{$bt['type']}{$bt['function']}($args)\n";
+           else if (isset($bt['type']))
+               $output .= "<b>call</b>: {$bt['type']}{$bt['function']}($args)\n";
+           else
+               $output .= "call: {$bt['function']}($args)\n";
+        }
+        $debug .= "<b>Backtrace</b>: \n".$output."\n";
     }
+
+    // display error page and exit
+    appdb_clear_buffer();
+    header("HTTP/1.1 500 Internal Server Error");
+    include(BASE."include/header.php");
+    echo "<h1>Ooops! Something has gone terribly wrong!</h1>\n";
+    echo "<p>Our monkey train has derailed! Worry not, a webmaster gopher help army has been dispatched and is on the way.</p>";
+    echo "<p>If this error continues to be a problem, please report it to us on our ";
+    echo "<a href=\"https://forum.winehq.org/\">Forums</a></p>\n";
+    echo "<hr>\n";
+    echo "<h3>error details:</h3>\n";
+    echo "<p><b>Error Message:</b> {$errstr}</p>\n";
+    echo "<p><b>File:</b> {$errfile}:{$errline}</p>\n";
+    if (!empty($debug)) {
+        echo "<hr>\n";
+        echo $debug;
+    }
+    include(BASE."include/footer.php");
+    exit();
+}
+
+/**
+ * clear the output buffer
+ */
+function appdb_clear_buffer()
+{
+    $status = ob_get_status();
+    if (!empty($status) and !empty($status['level']))
+    {
+        for ($c = 0; $c < ($status['level'] + 1); $c++)
+        {
+            @eval("ob_end_clean();");
+        }
+    }
+}
+
+/**
+ * return url with docroot prepended
+ */
+function apidb_url($path)
+{
+    return BASE.$path;
+}
+
+/**
+ * return FULL url with docroot prepended
+ */
+function apidb_fullurl($path = "")
+{
+    return BASE.$path;
+}
+
+/**
+ * return the full path of the calling script
+ */
+function appdb_fullpath($path)
+{
+    /* IE: we know this file is in /yyy/xxx/include, we want to get the /yyy/xxx 
+    /* so we call dirname  on this file path twice */
+    $fullpath = dirname(dirname(__FILE__))."//".$path;
+    /* get rid of potential double slashes due to string concat */
+    return str_replace("//", "/", $fullpath); 
+}
+
+/**
+ * output the common apidb header
+ */
+function apidb_header($title = 0)
+{
+    $realname = $_SESSION['current']->sRealname;
+
+    // Set Page Title
+    $page_title = $title;
+    if ($title)
+         $title = " - $title";
+
+    // Display Header
+    include(BASE."include/header.php");
+
+    // Display Sidebar
+    global $_APPDB_sidebar_func_list;
+    echo "<div id=\"sidebar\">\n<ul>\n";
+
+    // TURN on GLOBAL ADMIN MENU
+    if ($_SESSION['current']->hasPriv("admin"))
+    {
+        include(BASE."include/sidebar_admin.php");
+        apidb_sidebar_add("global_admin_menu");
+    } else if($_SESSION['current']->isMaintainer()) /* if the user maintains anything, add their menus */
+    {
+        include(BASE."include/sidebar_maintainer_admin.php");
+        apidb_sidebar_add("global_maintainer_admin_menu");
+    }
+
+    // Login Menu
+    include(BASE."include/sidebar_login.php");
+    apidb_sidebar_add("global_sidebar_login");
+
+    // Main Menu
+    include(BASE."include/sidebar.php");
+    apidb_sidebar_add("global_sidebar_menu");
+
+    //LOOP and display menus
+    for($i = 0; $i < sizeof($_APPDB_sidebar_func_list); $i++)
+    {
+        $func = $_APPDB_sidebar_func_list[$i];
+        $func();
+    }
+    echo "</ul>\n</div>\n";
+
+    // Display Status Messages
+    $GLOBALS['session']->dumpmsgbuffer();
+    if (is_array($GLOBALS['session']->msg) and count($GLOBALS['session']->msg) > 0)
+    {
+        echo html_frame_start("","300","",5);
+        foreach ($GLOBALS['session']->msg as $msg)
+        {
+            if ($msg['color'] == "red")
+               $msg['color'] = "{$msg['color']}; text-decoration: blink;";
+            echo "<div align=\"center\" class=\"session_note\" style=\"color: {$msg['color']};\"> {$msg['msg']} </div>";
+        }
+        echo html_frame_end("&nbsp;");
+        echo "<br>\n";
+    }
+}
+
+/**
+ * output the common apidb footer
+ */
+function apidb_footer()
+{
+    // log end time of script execution
+    appdb_debug("Ending Log");
+    appdb_debug("Script Execution time: ".round(microtime_float() - $GLOBALS['_APPDB_script_start_time'], 5)." seconds");
+
+    // Display Footer
+    if(!isset($header_disabled))
+        include(BASE."include/footer.php");
+}
+
+/**
+ * register a sidebar menu function
+ * the supplied function is called when the sidebar is built
+ */
+function apidb_sidebar_add($funcname)
+{
+    global $_APPDB_sidebar_func_list;
+    array_unshift($_APPDB_sidebar_func_list, $funcname);
+}
+
+/**
+ * msgs will be displayed on the Next page view of the same user
+ */
+function addmsg($shText, $color = "black")
+{
+    $GLOBALS['session']->addmsg($shText, $color);
 }
 
 ?>
--- include/incl.php
+++ include/incl.php
@@ -3,299 +3,67 @@
 /* Main Include Library for Application Database */
 /*************************************************/
 
-// get modules
-ini_set("memory_limit","64M");
+/**
+ * load required modules
+ */
 require_once(BASE."include/config.php");
+require(BASE."include/appdb.php");
 require(BASE."include/util.php");
 require(BASE."include/user.php");
 require(BASE."include/session.php");
-require(BASE."include/menu.php");
+require(BASE."include/htmlmenu.php");
 require(BASE."include/html.php");
 require(BASE."include/error_log.php");
 require(BASE."include/query.php");
 require(BASE."include/table.php");
 require_once(BASE."include/objectManager.php");
 
-/* if magic quotes are enabled make sure the user disables them */
-/* otherwise they will see all kinds of odd effects that are difficult */
-/* to track down */
-if(get_magic_quotes_gpc())
-{
-    echo "<b>Please disable the magic quotes GPC PHP setting. See <a href=\"http://us2.php.net/manual/en/ref.info.php#ini.magic-quotes-gpc\"> this page</a> for more information</b><br><br>";
-    echo "AppDB php code assumes magic quotes are disabled.<br><br>";
-    echo "Magic quotes are a bad idea for a few reasons.<br><br>";
-    echo "First is that php calls <b>addslashes()</b> on all \$_POST, \$_REQUEST and cookie variables ";
-    echo "if magic quotes is enabled. ";
-    echo "Ooooooh you say.<br>";
-    echo "<i>\"Aren't magic quotes a convienent way to protect my php code from sql injection attacks?\"</i><br><br>";
-    echo "No!  <b>addslashes()</b> isn't adequate.  You should use <b>query_escape_string()</b> or some other function";
-    echo " that will handle multi-byte characters.  See <a href=\"http://shiflett.org/archive/184\">this article</a>";
-    echo " for a way to exploit <b>addslash()</b>ed parameters.<br><br>";
-    echo "A second reason is that with magic quotes enabled, due to the use of <b>query_escape_string()</b> to";
-    echo " protect from sql injection attacks we'll end up with variables that have been addslash()ed and";
-    echo " <b>query_escape_string()</b>ed.  So you end up having to call stripslashes() on EVERY variable. ";
-    exit;
-}
+/**
+ * setup error handing
+ */
+set_error_handler('appdb_error_handler');
+register_shutdown_function('appdb_error_shutdown');
 
 /**
- * rename $_REQUEST variables to preserve backwards compatibility
- * with bugzilla links and urls in emails and on google from before our
- * mass rename of GPC variables to use our coding standard prefixing
- *
- * NOTE: we may be able to remove these backwareds compatibility changes
- *       in a few years, check in mid 2007 to see how many old
- *       links are still poping up in google then
+ * global vars
  */
-if(isset($_REQUEST['versionId']))
-{
-   $_REQUEST['iVersionId'] = $_REQUEST['versionId'];
-   unset($_REQUEST['versionId']);
-}
-if(isset($_REQUEST['appId']))
-{
-   $_REQUEST['iAppId'] = $_REQUEST['appId'];
-   unset($_REQUEST['appId']);
-}
-if(isset($_REQUEST['bug_id']))
-{
-    $_REQUEST['iBugId'] = $_REQUEST['bug_id'];
-    unset($_REQUEST['bug_id']);
-}
-if(isset($_REQUEST['catId']))
-{
-    $_REQUEST['iCatId'] = $_REQUEST['catId'];
-    unset($_REQUEST['catId']);
-}
-if(isset($_REQUEST['sub']))
-{
-    $_REQUEST['sSub'] = $_REQUEST['sub'];
-    unset($_REQUEST['sub']);
-}
-if(isset($_REQUEST['topic']))
-{
-    $_REQUEST['sTopic'] = $_REQUEST['topic'];
-    unset($_REQUEST['topic']);
-}
-if(isset($_REQUEST['mode']))
-{
-    $_REQUEST['sMode'] = $_REQUEST['mode'];
-    unset($_REQUEST['mode']);
-}
-/* End backwards compatibility code */
 
-// create arrays
-$sidebar_func_list = array();
-$help_list = array();
+// script execution start time
+$_APPDB_script_start_time = microtime_float();
 
-function apidb_help_add($desc, $id)
-{
-    global $help_list;
-    $help_list[] = array($desc, $id);
-}
+// sidebar function callbacks
+$_APPDB_sidebar_func_list = array();
 
+// enable debug flag
+$_APPDB_debug = false;
 
-// return url with docroot prepended 
-function apidb_url($path)
-{
-    return BASE.$path;
-}
-
-// return FULL url with docroot prepended
-function apidb_fullurl($path = "")
-{
-    return BASE.$path;
-}
-
-function appdb_fullpath($path)
-{
-    /* IE: we know this file is in /yyy/xxx/include, we want to get the /yyy/xxx 
-    /* so we call dirname  on this file path twice */
-    $fullpath = dirname(dirname(__FILE__))."//".$path;
-    /* get rid of potential double slashes due to string concat */
-    return str_replace("//", "/", $fullpath); 
-}
-
-
-/*
- * output the common apidb header
- */
-function apidb_header($title = 0)
-{
-    $realname = $_SESSION['current']->sRealname;
-
-    // Set Page Title
-    $page_title = $title;
-    if ($title)
-         $title = " - $title";
-
-    // grab the starting time
-    global $sPageGeneratingStartTime;
-    $sPageGeneratingStartTime = microtime();
-    $aStartarray = explode(" ", $sPageGeneratingStartTime);
-    $sPageGeneratingStartTime = $aStartarray[1] + $aStartarray[0]; 
-
-    // Display Header
-    include(BASE."include/header.php");
-
-    // Display Sidebar
-    apidb_sidebar();
-
-    // Display Status Messages
-    dumpmsgbuffer();
-}
-
-
-/*
- * output the common apidb footer
- */
-function apidb_footer()
-{
-    // grab the end of the page generating time
-    global $sPageGeneratingStartTime;
-    $sPageGeneratingEndTime = microtime();
-    $aEndarray = explode(" ", $sPageGeneratingEndTime);
-    $sPageGeneratingEndTime = $aEndarray[1] + $aEndarray[0];
-    $sTotaltime = $sPageGeneratingEndTime - $sPageGeneratingStartTime;
-    $sTotaltime = round($sTotaltime,5);
-    echo "<center>Page loaded in <b>$sTotaltime</b> seconds.</center>";
-
-    // Display Footer
-    if(!isset($header_disabled))
-        include(BASE."include/"."footer.php");
-}
-
-/*
- * output the sidebar, calls all functions registered with apidb_sidebar_add
- */
-function apidb_sidebar()
-{
-    global $sidebar_func_list;
-
-echo '
-<div id="sidebar">
-    <ul>
-';
-
-    //TURN on GLOBAL ADMIN MENU
-    if ($_SESSION['current']->hasPriv("admin"))
-    {
-        include(BASE."include/sidebar_admin.php");
-        apidb_sidebar_add("global_admin_menu");
-    } else if($_SESSION['current']->isMaintainer()) /* if the user maintains anything, add their menus */
-    {
-        include(BASE."include/sidebar_maintainer_admin.php");
-        apidb_sidebar_add("global_maintainer_admin_menu");
-    }
-
-    // Login Menu
-    include(BASE."include/sidebar_login.php");
-    apidb_sidebar_add("global_sidebar_login");
-
-    // Main Menu
-    include(BASE."include/sidebar.php");
-    apidb_sidebar_add("global_sidebar_menu");
-
-    //LOOP and display menus
-    for($i = 0; $i < sizeof($sidebar_func_list); $i++)
-    {
-        $func = $sidebar_func_list[$i];
-        $func();
-    }
-
-echo '
-    </ul>
-</div>
-';
-
-}
-
-
-/**
- * register a sidebar menu function
- * the supplied function is called when the sidebar is built
- */
-function apidb_sidebar_add($funcname)
-{
-    global $sidebar_func_list;
-    array_unshift($sidebar_func_list, $funcname);
-}
-
-
-function apidb_image($name)
-{
-    return BASE."images/$name";
-}
-
-
-/**
- * format a date as required for HTTP by RFC 2068 sec 3.3.1 
- */
-function fHttpDate($iDate) {
-   return gmdate("D, d M Y H:i:s",$iDate)." GMT";
-}
-
-/**
- *  parse all the date formats required by HTTP 1.1 into PHP time values
- */
-function pHttpDate($sDate) {
-   $iDate = strtotime($sDate);
-   if ($iDate != -1) return $iDate;
-		/* the RFC also requires asctime() format... */
-   $aTs = strptime($sDate,"%a %b  %e %H:%M:%S %Y");
-   $iDate = gmmktime($aTs[2],$aTs[1],$aTs[0],$aTs[4],$aTs[3],$aTs[5],0);
-   return $iDate;
-}
-
-/**
- * msgs will be displayed on the Next page view of the same user
- */
-function addmsg($shText, $color = "black")
-{
-    $GLOBALS['session']->addmsg($shText, $color);
-}
-
-
-function purgeSessionMessages()
-{
-  $GLOBALS['session']->purgemsg();
-}
-
-
-/**
- * output msg_buffer and clear it.
- */
-function dumpmsgbuffer()
-{
-    $GLOBALS['session']->dumpmsgbuffer();
-    if (is_array($GLOBALS['session']->msg) and count($GLOBALS['session']->msg) > 0)
-    {
-        echo html_frame_start("","300","",5);
-        foreach ($GLOBALS['session']->msg as $msg)
-        {
-            if ($msg['color'] == "red")
-               $msg['color'] = "{$msg['color']}; text-decoration: blink;";
-            echo "<div align=\"center\" class=\"session_note\" style=\"color: {$msg['color']};\"> {$msg['msg']} </div>";
-        }
-        echo html_frame_end("&nbsp;");
-        echo "<br>\n";
-    }
-}
+// debug log
+$_APPDB_debugLog = "";
 
 /**
  * Init Session (stores user info in session)
  */
+
 $session = new session("whq_appdb");
 $session->register("current");
-
 if(!isset($_SESSION['current']))
 {
     $_SESSION['current'] = new User();
 }
 
-// if we are debugging we need to see all errors
-if($_SESSION['current']->showDebuggingInfos()) error_reporting(E_ALL ^ E_NOTICE);
-
 // include filter.php to filter all REQUEST input
 require(BASE."include/filter.php");
+
+/**
+ * turn on debuging admin pref
+ */
+
+if($_SESSION['current']->showDebuggingInfos())
+    $_APPDB_debug = true;
+
+/**
+ * Start Debug Log
+ */
+appdb_debug("Starting Debug Log");
 
 ?>
