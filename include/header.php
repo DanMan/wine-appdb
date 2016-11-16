4fe237e96cb9df7ca88bdebdb8502a2bc053090b
Jeremy Newman <jnewman@codeweavers.com>
2016-11-16 10:52:13 -0600

    merge all the stylesheets, clean up the css for tables and frames
     try to repicate the old look for now on tables, as bootstrap really
     changes things up.
    
     add some frame_table classes for the changes in html.php

--- include/header.php
+++ include/header.php
@@ -18,9 +18,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" type="text/css" media="all">
-    <link rel="stylesheet" href="<?php echo BASE; ?>styles.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="<?php echo BASE; ?>apidb.css" type="text/css">
-    <link rel="stylesheet" href="<?php echo BASE; ?>application.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo BASE; ?>styles.css" type="text/css" media="all">
 
     <script src="https://code.jquery.com/jquery-2.2.3.min.js" type="text/javascript"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
