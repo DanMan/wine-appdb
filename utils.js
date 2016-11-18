4453a761313826324432953a655a3a40a57fb271
Jeremy Newman <jnewman@codeweavers.com>
2016-11-18 15:38:31 -0600

    remove openWin, no longer used

--- utils.js
+++ utils.js
@@ -62,7 +62,6 @@ $(document).ready(function()
         $(this).addClass('cursor-pointer');
         $(this).click(function(e)
         {
-
             document.location = sURL;
         });
     });
@@ -108,21 +107,5 @@ $.fn.toggleClick = function(){
             $(this).data('iteration', iteration);
     });
 };
-
-// open Window (FIXME: replace with an inline screenshot viewer)
-function openWin(fileToOpen,nameOfWindow,width,height) {
-    myWindow = window.open("",nameOfWindow,"menubar=no,scrollbars=yes,status=no,width="+width+",height="+height);
-    myWindow.document.open();
-    myWindow.document.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">');
-    myWindow.document.write('<html><head><title>Screenshot Viewer</title>')
-    myWindow.document.write('<style type="text/css">');
-    myWindow.document.write('body { margin: 0; padding: 0; background-color: lightgrey; }');
-    myWindow.document.write('img { border: 0; }');
-    myWindow.document.write('p { display: inline; }');
-    myWindow.document.write('</style></head><body>');
-    myWindow.document.write('<a onclick="self.close();" href=""><img src="'+ fileToOpen +'" alt="Screenshot"></a>');
-    myWindow.document.write('</body></html>');
-    myWindow.document.close();
-}
 
 // done
