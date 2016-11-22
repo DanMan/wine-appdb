b93a287d35ee2d18c7eaf30ca366f6ca5f3e91a1
Jeremy Newman <jnewman@codeweavers.com>
2016-11-22 09:30:26 -0600

    use minified version of jquery-ui theme

--- include/header.php
+++ include/header.php
@@ -17,7 +17,7 @@
 
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" type="text/css" media="all">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css" type="text/css" media="all">
     <link rel="stylesheet" href="<?php echo BASE; ?>styles.css" type="text/css" media="all">
 
     <script src="https://code.jquery.com/jquery-2.2.3.min.js" type="text/javascript"></script>
