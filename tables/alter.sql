160b16a39334d26186f3d56875216c6a744ea43b
Jeremy Newman <jnewman@codeweavers.com>
2017-06-05 15:55:13 -0500

    changes went live

--- tables/alter.sql
+++ tables/alter.sql
@@ -1,3 +1,2 @@
 /* append to this file when changes are required to the live db */
 /* it will be cleared when the changes go live */
-ALTER TABLE testResults ADD column staging tinyint(1) not null default '0' AFTER testedRelease;
