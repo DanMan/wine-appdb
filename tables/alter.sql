d311fde7358ebd03ec65f23461de91ff79f59813
Jeremy Newman <jnewman@codeweavers.com>
2017-08-25 09:12:01 -0500

    take live

--- tables/alter.sql
+++ tables/alter.sql
@@ -1,3 +1,2 @@
 /* append to this file when changes are required to the live db */
 /* it will be cleared when the changes go live */
-ALTER TABLE testResults ADD COLUMN gpuMfr ENUM('AMD', 'Intel', 'Nvidia', 'Other', 'Unknown') DEFAULT NULL, ADD COLUMN graphicsDriver ENUM('open source', 'proprietary', 'unknown') DEFAULT NULL;
